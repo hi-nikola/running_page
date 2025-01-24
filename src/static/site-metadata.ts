aebe3c3d2318bbc9b75fe218e597a7a20af6769e
hi_nikola <344382052@qq.com>
2025-01-24 19:47:18 +0800

    Reinitialized repository

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,29 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Running Page',
+  siteUrl: 'https://hi-nikola.run',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Blog111',
+      url: 'https://github.com/yihong0618/gitblog',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+    },
+  ],
+};
+
+export default data;
